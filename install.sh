e7f01b3b5b2e9ab1b8405979633373028c64638c
NATEE <natee@iavar.one>
2022-12-26 13:14:14 +0100

    install apt libraries

--- install.sh
+++ install.sh
@@ -9,4 +9,6 @@ npm i --prefix ./shells/sveltkit-electron
 
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install -y wine64 wine32 libnss3-dev libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0
+sudo apt install -y wine64 wine32
+sudo apt install -y libnss3-dev libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0
+sudo apt install -y xserver-xorg-video-all xserver-xorg-input-all xserver-xorg-core xinit x11-xserver-utils
