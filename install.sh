ef1b3417b5d3704c0bbcd3d7361798ebafc143fb
NATEE <natee@iavar.one>
2022-12-26 12:58:46 +0100

    install shared libraries

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 git config --global --add safe.directory '*'
 
+npm i -g cross-env
 npm i
 
 git submodule update --init
@@ -8,4 +9,4 @@ npm i --prefix ./shells/sveltkit-electron
 
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install -y wine64 wine32 libnss3
+sudo apt install -y wine64 wine32 libnss3-dev libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0
