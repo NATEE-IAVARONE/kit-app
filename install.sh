7260bed6a09d4cf84866b15bd8f8017838a562bd
NATEE <natee@iavar.one>
2022-12-26 12:24:04 +0100

    install wine 32/64 and libnss3

--- install.sh
+++ install.sh
@@ -2,4 +2,10 @@ git config --global --add safe.directory '*'
 
 npm i
 
+git submodule update --init
+
 npm i --prefix ./shells/sveltkit-electron
+
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install -y wine64 wine32 libnss3
