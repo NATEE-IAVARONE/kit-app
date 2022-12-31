e4717ff8504aa8107f72ef95a24c6340e9931b2f
NATEE <natee@iavar.one>
2022-12-31 11:32:23 +0100

    install and implement sveltekit-adapter-chrome-extension

--- shells/chrome-ext/svelte.config.js
+++ shells/chrome-ext/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from 'sveltekit-adapter-chrome-extension';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,16 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+      // default options are shown
+      pages: "build",
+      assets: "build",
+      fallback: null,
+      precompress: false,
+      manifest: "manifest.json",
+      emptyOutDir: true,
+    }),
+		appDir: 'app'
 	}
 };
 
